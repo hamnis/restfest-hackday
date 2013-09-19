0ede6f8a9b0fdaaf213556804399047ca9c82235
Erlend Hamnaberg <erlend@hamnaberg.net>
2013-09-19 15:26:14 -0400

    Send post complete and fail if they exist.

--- src/main/scala/restfest/Main.scala
+++ src/main/scala/restfest/Main.scala
@@ -7,28 +7,65 @@ import dispatch._
 import Defaults._
 import workorder.WorkOrder
 import java.net.URI
-import scala.concurrent.Await
-import scala.concurrent.duration._
+import com.ning.http.client.Response
 
 
 object Main extends App {
-  val uri = URI.create(args(0))
+  val latch = new java.util.concurrent.CountDownLatch(1)
+  work(URI.create(args(0)))
 
-  def getWorkItems = for {
-    json <- Http(url(uri.toString) OK as.String)
-    items <- Future { JsonCollection.parse(json).right.toOption.map(_.items).getOrElse(Nil) }
-    wo <- Future.sequence(items.map(i => Http(url(uri.resolve(i.href).toString) OK as.String)))
-  } yield wo.map(w => WorkOrder.parse(w).toOption).flatMap(_.toSeq)
+  latch.await()
+  sys.exit(0)
 
-  val sounds = for {
-    items <- getWorkItems
-  } yield items.find(wo => wo._type == Player.Type)
+  private def work(implicit base: URI) {
+    val workItems = for {
+      json <- Http(url(base.toString) OK as.String)
+      items <- Future { JsonCollection.parse(json).right.toOption.map(_.items).getOrElse(Nil) }
+      wo <- Future.sequence(items.map(i => Http(url(base.resolve(i.href).toString) OK as.String)))
+    } yield wo.map(w => WorkOrder.parse(w).toOption).flatMap(_.toSeq)
 
-  val result = Await.result(sounds, 10.seconds)
+    val firstSound = for {
+      items <- workItems
+    } yield items.find(wo => wo._type == Player.Type)
 
-  result.map(wo => wo.as[Sound]).foreach{ s =>
-    val res = Player.create(uri.resolve(s.href), s.volume)
-    res.play()
+    for {
+      ss <- firstSound
+      res <- ss.flatMap(_.start).map(h => sendPost(h).map(_.getStatusCode)).getOrElse(Future(200))
+      _ <- doPlay(res, ss)
+    } yield { () }
   }
 
+
+  private def doPlay(res: Int, workOrder: Option[WorkOrder])(implicit base: URI): Future[Unit] = {
+    Future {
+      if (res == 200) {
+        for {
+          wo <- workOrder
+          clip <- {
+            val sound = wo.as[Sound]
+            Some(Player.create(base.resolve(sound.href), sound.volume))
+          }
+          _ <- {
+            clip.play()
+            while(clip.isPlaying){ Thread.sleep(100)}
+            Some(())
+          }
+          complete <- wo.complete
+        } yield {
+          sendPost(complete).onComplete(_ => latch.countDown())
+        }
+      } else {
+        for {
+          wo <- workOrder
+          fail <- wo.fail
+        } yield {
+          sendPost(fail).onComplete(_ => latch.countDown())
+        }
+      }
+    }
+  }
+
+  private def sendPost(h: URI)(implicit base: URI): dispatch.Future[Response] = {
+    Http(url(base.resolve(h).toString).setMethod("POST"))
+  }
 }
